9586d6bc9a17c2d5ebec0fdba03b67520bfda219
Tom Bestebreurtje <tombestebreurtje@funda.nl>
2015-11-29 20:46:54 +0100

    add locally operating heatmap

--- index.js
+++ index.js
@@ -33,10 +33,17 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 app.get('/', function(request, response) {
   response.sendfile(__dirname + '/public/index.html');
 });
 
+
+// REST API
+// --------
 app.get('/cool', function(request, response) {
   var result = '';
   var times = process.env.TIMES || 1;
@@ -45,29 +52,33 @@ app.get('/cool', function(request, response) {
   response.send(result);
 });
 
-app.get('/things', function(req, res) {
-  Thing.find(function (err, things) {
-    if (err) return console.error(err);
-    res.send(things);
-  })
-});
+app.route('/things')//todo: /:thingId
+    .get(function(req, res) {
+      Thing.find(function(err, things) {
+        if (err) return console.error(err);
+        res.send(things);
+      });
+    })
+    .post(function(req, res) {
+      console.log('Express: /things POST: ', req.body);
+      var newThing = new Thing(
+          {
+            key: req.body.key,
+            value: req.body.value
+          }
+      );
+      newThing.save(function(err, thing) {
+        if (err) console.log(err);
+        console.log('Saved a thing!');
+        res.json(req.body)
+      });
+    })
+    .delete(function(req, res) {
+      console.log('Express: /things DELETE');
+      Thing.remove({}, function(err) {
+        if (err) console.log(err);
+        res.send('Deleted all the things');
+      });
+    });
 
-app.post('/things', function(req, res) {
-  console.log('Express: /things POST: ', req.body);
-  //handle POST
-  var newThing = new Thing(
-      {
-        key: req.body.key,
-        value: req.body.value
-      }
-  );
-  newThing.save(function(err, thing) {
-    if (err) console.log(err);
-    console.log('Saved a thing!');
-    res.json(req.body)
-  });
-});
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
--- public/script.js
+++ public/script.js
@@ -1,35 +1,76 @@
-console.log('hi world');
+/*
+-modularize this
+-modularize simpleHeat.js
+-...
+ */
 
-var target = document.querySelector('.target');
-var coolButton = document.querySelector('button');
+var logSpace = document.querySelector('.log-space');
+var coolButton = document.querySelector('.cool-button');
 var postButton = document.querySelector('.post-button');
+var deleteButton = document.querySelector('.delete-button');
+
+var heatCanvas = document.querySelector('#canvas');
+
 coolButton.addEventListener('click', doSomethingCool);
 postButton.addEventListener('click', doPost);
+deleteButton.addEventListener('click', doDelete);
 
 function doSomethingCool() {
   var oReq = new XMLHttpRequest();
-  oReq.addEventListener('load', handler);
+  oReq.addEventListener('load', resultToScreenHandler);
   oReq.open('GET', '/cool');
   oReq.send();
 }
 
-var thing = {
-  key: '',
-  value: ''
-};
-
 function doPost() {
   var oReq = new XMLHttpRequest();
+  var thing = {};
   thing.key = 'mahKey';
   thing.value = 'mahValue';
 
-  oReq.addEventListener('load', handler);
+  oReq.addEventListener('load', resultToScreenHandler);
   oReq.open('POST', '/things');
   oReq.setRequestHeader('Content-Type', 'application/json');
   oReq.send(JSON.stringify(thing));
 }
 
-function handler() {
-  console.log('handler: ', this);
-  target.innerHTML += this.responseText + ' ';
+function doDelete() {
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', resultToScreenHandler);
+  oReq.open('DELETE', '/things');
+  oReq.send();
 }
+
+function resultToScreenHandler() {
+  console.log('handler: ', this);
+  logSpace.innerHTML += this.responseText + '<br>';
+}
+
+
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+heatCanvas.width= screen.width;
+heatCanvas.height= screen.height;
+var heat = SimpleHeat('canvas').max(18),
+    frame;
+
+function draw() {
+  heat.draw();
+  frame = null;
+}
+draw();
+window.onmousemove = function(e) {
+  heat.add([e.layerX, e.layerY, 1]);
+  frame = frame || window.requestAnimationFrame(draw);
+};
+
+var radius = document.querySelector('#radius'),
+    blur = document.querySelector('#blur'),
+    changeType = 'oninput' in radius ? 'oninput' : 'onchange';
+
+radius[changeType] = blur[changeType] = function(e) {
+  heat.radius(+radius.value, +blur.value);
+  frame = frame || window.requestAnimationFrame(draw);
+};
+
