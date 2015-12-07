c9567b3840c9c24752eb8e595a1e28b15fdb7d38
Tom Bestebreurtje <tombestebreurtje@funda.nl>
2015-12-07 20:41:03 +0100

    Socket used for all heat data + cleanup

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
-var dbUriString;
+var dbUriString = process.env.MONGOLAB_URI || 'mongodb://localhost:27017';
 
+var app;
 var server;
 var io;
-var app = express();
 
-// Static page
+// APP STARTUP
 // -----------
+app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-
 server = require('http').Server(app);
 io = require('socket.io')(server);
 
@@ -22,21 +22,14 @@ server.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+// no fancy templates, just a static page
 app.get('/', function(request, response) {
   response.sendfile(__dirname + '/public/index.html');
 });
 
-io.on('connection', function (socket) {
-  //socket.emit('connected', { hello: 'world' });
-  //socket.on('heatAdded', function (data) {
-  //  console.log('heatAdded: ', data);
-  //});
-});
 
-
-// DB
-// --
-dbUriString = process.env.MONGOLAB_URI || 'mongodb://localhost:27017';
+// MONGO CONNECTION + MONGOOSE SCHEMA
+// ----------------------------------
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
@@ -56,9 +49,27 @@ var heatPointSchema = new mongoose.Schema({
 var HeatPoint = mongoose.model('HeatPoint', heatPointSchema);
 
 
-// REST API
-// --------
+// SOCKET.IO
+// ---------
+io.on('connection', function(socket) {
+
+  dbGetHeatData().then(function(data) {
+    //send complete heat set to newly opened socket
+    socket.emit('totalHeat', data);
+  });
+
+  socket.on('clientHeat', function(data) {
+    dbSaveThing(data).then(function() {
+      io.emit('serverHeat', data);
+    });
+  });
+});
+
+
+// REST API/ROUTER
+// ---------------
 app.get('/cool', function(request, response) {
+  // where the MEAN adventure began..
   var result = '';
   var times = process.env.TIMES || 1;
   for (var i = 0; i < times; i++)
@@ -67,29 +78,24 @@ app.get('/cool', function(request, response) {
 });
 
 app.route('/heat')
-
+  // can use this to get the raw heat JSON.
     .get(function(req, res) {
       dbGetHeatData().then(function(data) {
         res.json(data);
       })
     })
 
-    .post(function(req, res) {
-      console.log('Express: /heat POST: ', req.body);
-      dbSaveThing(req).then(function() {
-        io.emit('heatAdded', req.body);
-        res.json(req.body)
-      });
-    })
-
     .delete(function(req, res) {
+      // use with care :"|
       console.log('Express: /heat DELETE');
       dbDeleteHeat().then(function() {
         res.send('Deleted all the things');
       });
-    })
-;
+    });
 
+
+// DB operations
+// -------------
 function dbGetHeatData() {
   return HeatPoint.find().exec(function(err, docs) {
     if (err) console.log(err);
@@ -100,12 +106,12 @@ function dbGetHeatData() {
   });
 }
 
-function dbSaveThing(data) {
+function dbSaveThing(point) {
   var newThing = new HeatPoint(
       {
-        x: data.body[0],
-        y: data.body[1],
-        opacity: data.body[2]
+        x: point[0],
+        y: point[1],
+        opacity: point[2]
       }
   );
   return newThing.save(function(err, thing) {
--- public/script.js
+++ public/script.js
@@ -1,105 +1,77 @@
-/*
--modularize this
--modularize simpleHeat.js
--...
-- draw on mousedown + on drag
- */
-
 var logSpace = document.querySelector('.log-space');
 var coolButton = document.querySelector('.cool-button');
-var postButton = document.querySelector('.post-button');
 var deleteButton = document.querySelector('.delete-button');
-
 var heatCanvas = document.querySelector('#canvas');
 var heatMap;
 var frame;
 var radiusSetting = document.querySelector('#radius');
 var blurSetting = document.querySelector('#blur');
 var browserChangeEvent = 'oninput' in radiusSetting ? 'oninput' : 'onchange';
-
 var POINT_OPACITY = 1;
-
 var socket = io.connect('http://localhost:5000');
 
-socket.on('connected', function (data) {
-  console.log('connected: ', data);
-  socket.emit('my other event', { my: 'data' });
-});
+function setupCanvasAndAddBindings() {
+  heatCanvas.width= screen.width;
+  heatCanvas.height= screen.height;
 
-socket.on('heatAdded', function(data) {
-  console.log('-TB- socket.on heatAdded: ', data);
-  heatMap.add(data);
-  frame = frame || window.requestAnimationFrame(draw);
-});
+  window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+      window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
-heatCanvas.width= screen.width;
-heatCanvas.height= screen.height;
-getHeatData();
+  //event bindings that technically could use the socket, but left as AJAX here
+  coolButton.addEventListener('click', doSomethingCool);
+  deleteButton.addEventListener('click', deleteHeatData);
 
-window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  window.onclick = function(e) {
+    var point = [e.layerX, e.layerY, POINT_OPACITY];
+    socket.emit('clientHeat', point);
+  };
 
-//events
-coolButton.addEventListener('click', doSomethingCool);
-deleteButton.addEventListener('click', deleteHeatData);
+  radiusSetting[browserChangeEvent] = blurSetting[browserChangeEvent] = function(e) {
+    heatMap.radius(+radiusSetting.value, +blurSetting.value);
+    frame = frame || window.requestAnimationFrame(renderHeat);
+  };
+}
 
-window.onclick = function(e) {
-  var point = [e.layerX, e.layerY, POINT_OPACITY];
-  doPoint(point);
-  frame = frame || window.requestAnimationFrame(draw);
-};
 
-radiusSetting[browserChangeEvent] = blurSetting[browserChangeEvent] = function(e) {
-  heatMap.radius(+radiusSetting.value, +blurSetting.value);
-  frame = frame || window.requestAnimationFrame(draw);
-};
-
-//requests
+// AJAX requests
 function doSomethingCool() {
   var oReq = new XMLHttpRequest();
-  oReq.addEventListener('load', resultToScreenHandler);
+  oReq.addEventListener('load', addToLog);
   oReq.open('GET', '/cool');
   oReq.send();
 }
 
-/**
- * POST a point
- * @param {array} point - [x ,y, opacity]
- */
-function doPoint(point) {
-  var oReq = new XMLHttpRequest();
-  oReq.addEventListener('load', resultToScreenHandler);
-  oReq.open('POST', '/heat');
-  oReq.setRequestHeader('Content-Type', 'application/json');
-  oReq.send(JSON.stringify(point));
-}
-
 function deleteHeatData() {
   var oReq = new XMLHttpRequest();
-  oReq.addEventListener('load', resultToScreenHandler);
+  oReq.addEventListener('load', addToLog);
   oReq.open('DELETE', '/heat');
   oReq.send();
 }
 
-function getHeatData() {
-  var oReq = new XMLHttpRequest();
-  oReq.addEventListener('load', initHeat);
-  oReq.open('GET', '/heat');
-  oReq.send();
-}
-
-function initHeat() {
-  var data = JSON.parse(this.responseText);
+// SOCKET.IO
+// ---------
+socket.on('totalHeat', function(data) {
+  console.log('totalHeat: ', data);
   heatMap = SimpleHeat('canvas').data(data).max(4);
-  heatMap.draw();
-}
+  frame = frame || window.requestAnimationFrame(renderHeat);
+});
+
+socket.on('serverHeat', function(data) {
+  console.log('serverHeat: ', data);
+  heatMap.add(data);
+  frame = frame || window.requestAnimationFrame(renderHeat);
+});
+
 
 //rendering
-function resultToScreenHandler() {
+function addToLog() {
   logSpace.innerHTML += '<span class="log">' + this.responseText + '</span>';
 }
 
-function draw() {
+function renderHeat() {
   heatMap.draw();
   frame = null;
 }
+
+// Module pattern? Ain't nobody got time for that!
+setupCanvasAndAddBindings();
